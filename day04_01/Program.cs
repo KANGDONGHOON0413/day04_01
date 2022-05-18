ee1a45bde4e37a4f535fdca6626517a9de4b6471
KANGDONGHOON0413 <kmerits@gmail.com>
2022-05-18 13:37:20 +0900

    초기 커밋.

---
+++ day04_01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day04_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = submain01();
+            foreach(var i in p)
+            {
+                Console.WriteLine(i);
+            }
+
+            foreach (var i in submain02())
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static int[] submain01() {
+           int[] p = new int[] { 10, 20, 30 };
+            return p;
+        }
+        static IEnumerable<int> submain02()
+        {
+            int[] p = new int[] { 10, 20, 30,40,50 };
+            for(int i = 0; i < p.Length; i++)
+            {
+                if (p[i] == 50) yield break;
+                yield return p[i];
+                Console.WriteLine((i+1) + "번째 종료");
+            }
+            
+        }
+
+    }
+}
